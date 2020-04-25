93e03685d30622b8477f0a301a559687c2739494
benjamin.jin <benjamin.jin@sifive.com>
2020-04-25 14:55:59 +0900

    Modify Cd - find root

--- src/com/rtjvm/scala/oop/commands/Cd.scala
+++ src/com/rtjvm/scala/oop/commands/Cd.scala
@@ -1,3 +1,16 @@
 package com.rtjvm.scala.oop.commands
 
-class Cd ()
+import com.rtjvm.scala.oop.filesystem.State
+
+class Cd (dir: String) extends Command {
+  override def apply(state: State): State = {
+    // 1. find root
+    val root = state.root
+    val wd = state.wd
+    // 2. find absolute path of directory
+
+    // 3. find the directory
+
+    // 4. change the state
+  }
+}
